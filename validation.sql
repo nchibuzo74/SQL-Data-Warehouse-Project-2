023ca320419ecaecc785ebf0e58097666e849c0a
Chibuzo Nwankwo <55243830+nchibuzo74@users.noreply.github.com>
2026-01-02 00:02:54 +0100

    Add validation and drop statements for tables/views
    This SQL script drops existing tables and views in the Bronze, Silver, and Gold layers, and validates the data by selecting from each layer.

---
+++ validation.sql
@@ -0,0 +1,67 @@
+-------------------------------------
+drop table bronze.crm_sales_details_staging;
+drop table bronze.crm_prd_info_staging;
+drop table bronze.crm_cust_info_staging;
+drop table bronze.erp_loc_a101_staging;
+drop table bronze.erp_cust_az12_staging;
+drop table bronze.erp_px_cat_g1v2_staging;
+
+drop table bronze.crm_sales_details;
+drop table bronze.crm_prd_info;
+drop table bronze.crm_cust_info;
+drop table bronze.erp_loc_a101;
+drop table bronze.erp_cust_az12;
+drop table bronze.erp_px_cat_g1v2;
+
+drop table silver.crm_sales_details;
+drop table silver.crm_prd_info;
+drop table silver.crm_cust_info;
+drop table silver.erp_loc_a101;
+drop table silver.erp_cust_az12;
+drop table silver.erp_px_cat_g1v2;
+
+drop view gold.fact_sales;
+drop view gold.dim_products;
+drop view gold.dim_customers;
+----------------------------------------------------------------------
+
+
+-----Validate Bronze, Silver and Gold Tables/Views-----
+----------------------------------------------------------
+------------------Bronze Layer---------------------------
+select * from bronze.crm_cust_info limit 5;
+select * from bronze.crm_prd_info limit 5;
+select * from bronze.crm_sales_details limit 5;
+select * from bronze.erp_loc_a101 limit 5;
+select * from bronze.erp_cust_az12 limit 5;
+select * from bronze.erp_px_cat_g1v2 limit 5;
+
+----------------Bronze Staging------------------------------------
+select * from bronze.crm_cust_info_staging;
+select * from bronze.crm_cust_info_staging;
+select * from bronze.crm_cust_info_staging
+
+-----------------Gold Views-------------------------------------
+select * from gold.dim_customers;
+select * from gold.dim_products;
+select * from gold.dim_sales;
+
+-----------------Gold Tables-------------------------------------
+select * from gold.customers;
+select * from gold.products;
+select * from gold.sales;
+
+-----------------Silver Tables-------------------------------------
+select * from silver.crm_cust_info;
+select * from silver.crm_prd_info;
+select * from silver.crm_sales_details;
+select * from silver.erp_loc_a101;
+select * from silver.erp_cust_az12;
+select * from silver.erp_px_cat_g1v2;
+
+------------------------------------------------
+select * 
+from silver.crm_sales_details
+where sls_prd_key = 'BB-7421';
+
+--------------------------------------------
